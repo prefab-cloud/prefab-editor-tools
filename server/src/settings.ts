2d891fdc51666628a4b404d8ac94d5ae255ad5ea
Jeffrey Chupp <jeff@semanticart.com>
2023-10-04 13:19:19 -0400

    Support .env file
    At the moment, though, we only support one prefab instance at a time. So
    you can't have multiple projects open at the same time yet.

--- server/src/settings.ts
+++ server/src/settings.ts
@@ -2,6 +2,9 @@ import { Connection } from "vscode-languageserver/node";
 import { prefabInit } from "./prefabClient";
 import { type Logger, type Settings } from "./types";
 
+import * as fs from "fs";
+import * as path from "path";
+
 let lastApiKeyWarning = 0;
 
 const warnAboutMissingApiKey = (connection: Connection) => {
@@ -23,7 +26,36 @@ const getSettings = async (
   log: Logger,
   refresh: () => Promise<void>
 ) => {
+  const workspaceFolders = await connection.workspace.getWorkspaceFolders();
+
+  let apiKey: string | undefined = undefined;
+
+  log("Lifecyle", { getSettings: { workspaceFolders } });
+
+  workspaceFolders?.forEach((folder) => {
+    if (folder.uri.startsWith("file://") && !apiKey) {
+      const envFile = path.join(folder.uri.split("file://")[1], `.env`);
+      if (fs.existsSync(envFile)) {
+        fs.readFileSync(envFile, "utf8")
+          .split("\n")
+          .forEach((line) => {
+            if (line.startsWith("PREFAB_API_KEY=")) {
+              apiKey = line.split("PREFAB_API_KEY=")[1];
+
+              log("Settings", `Pulled API key from .env file ${envFile}`);
+            }
+          });
+      }
+    }
+  });
+
   const newSettings = await connection.workspace.getConfiguration("prefab");
+
+  if (apiKey) {
+    newSettings.apiKey = apiKey;
+  } else {
+    log("Settings", "Using API key from settings");
+  }
 
   updateSettings(connection, newSettings, log, refresh);
 };
@@ -35,9 +67,7 @@ const updateSettings = (
   refresh: () => Promise<void>
 ) => {
   if ((!newSettings || !newSettings.apiKey) && !settings.apiKey) {
-    connection.console.error(
-      "No API key set. Please update your configuration."
-    );
+    connection.console.error("No API key set. Please update your settings.");
     warnAboutMissingApiKey(connection);
   }
 
